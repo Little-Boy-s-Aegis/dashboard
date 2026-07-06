5f10fa740b9b38e69e0e31229a208243928321dd
L1nkinPark <hieuvdfs91539@fpt.edu.vn>
2026-07-06 14:44:47 +0700

    feat: align Go LogEntry models with ECS schema specification

--- backend/models/models.go
+++ backend/models/models.go
@@ -64,6 +64,18 @@ type LogEntry struct {
 	ThreatFlagged  bool      `json:"threatFlagged,omitempty"`
 	ThreatType     string    `json:"threatType,omitempty"`
 	DecodedPayload string    `json:"decodedPayload,omitempty"`
+
+	// ECS (Elastic Common Schema) Standard Fields
+	ECSTimestamp   string    `json:"@timestamp"`
+	ECSLogLevel    string    `json:"log.level"`
+	ECSEventDataset string   `json:"event.dataset"`
+	ECSEventID     string    `json:"event.id"`
+	ECSSourceIP    string    `json:"source.ip"`
+	ECSHTTPStatus  int       `json:"http.response.status_code,omitempty"`
+	ECSGeoCountry  string    `json:"source.geo.country_name,omitempty"`
+	ECSASNName     string    `json:"source.as.organization.name,omitempty"`
+	ECSServiceName string    `json:"service.name"`
+	ECSURLOriginal string    `json:"url.original,omitempty"`
 }
 
 type AIAnalysis struct {
